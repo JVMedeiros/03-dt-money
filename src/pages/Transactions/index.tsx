55ab484a7b890ee397c8043b572d92cf1ecc886e
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-07-12 11:50:11 -0300

    wip: transaction table wip

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,40 @@
 import { Header } from "../../components/Header/index";
 import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionsContainer, TransactionsMainContainer, TransactionsTable, TransactionsTableBody, TransactionsTableData, TransactionsTableRow } from "./styles";
 
 export function Transactions() {
   return (
-    <div>
+    <TransactionsMainContainer>
       <Header />
       <Summary />
-    </div>
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <TransactionsTableBody>
+            <TransactionsTableRow>
+              <TransactionsTableData width="40%">Desenvolvimento de site</TransactionsTableData>
+              <TransactionsTableData>
+                <PriceHighlight variant="income">
+                  R$ 12.000,00
+                </PriceHighlight>
+              </TransactionsTableData>
+              <TransactionsTableData>Venda</TransactionsTableData>
+              <TransactionsTableData>13/04/2022</TransactionsTableData>
+            </TransactionsTableRow>
+
+            <TransactionsTableRow>
+              <TransactionsTableData width="40%">Hamburguer</TransactionsTableData>
+              <TransactionsTableData>
+                <PriceHighlight variant="outcome">
+                  - R$ 59,00
+                </PriceHighlight>
+              </TransactionsTableData>
+              <TransactionsTableData>Alimentação</TransactionsTableData>
+              <TransactionsTableData>10/04/2022</TransactionsTableData>
+            </TransactionsTableRow>
+          </TransactionsTableBody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </TransactionsMainContainer>
   )
 }
