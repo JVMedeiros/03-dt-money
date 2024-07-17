a2adc01d4d5e630785cd82f122048ebae6b0f113
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-07-16 22:24:56 -0300

    wip: summary component function wip

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,7 +1,23 @@
 import { SummaryCard, SummaryContainer, SummaryHeader, SummarySpan, SummaryTextContent } from "./styles";
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react';
+import { useContext } from "react";
+import { TransactionsContext } from "../../context/TransactionContext";
 
 export function Summary() {
+  const { transactions } = useContext(TransactionsContext)
+  const summary = transactions.reduce((acc, transaction) => {
+    if(transaction.type === 'income') {
+      acc.income += transaction.price
+      acc.total += transaction.price
+    } else {
+      acc.outcome += transaction.price
+      acc.total -= transaction.price
+
+    }
+    
+    return acc;
+  }, { income: 0, outcome: 0, total: 0 })
+
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -9,7 +25,7 @@ export function Summary() {
           <SummarySpan>Entradas</SummarySpan>
           <ArrowCircleUp size={32} color="#00b37e"/>
         </SummaryHeader>
-          <SummaryTextContent>R$ 17.400,00</SummaryTextContent>
+          <SummaryTextContent>{summary.income}</SummaryTextContent>
       </SummaryCard>
 
       <SummaryCard>
@@ -17,7 +33,7 @@ export function Summary() {
           <SummarySpan>Saídas</SummarySpan>
           <ArrowCircleDown size={32} color="#f75a68"/>
         </SummaryHeader>
-          <SummaryTextContent>R$ 7.400,00</SummaryTextContent>
+          <SummaryTextContent>{summary.outcome}</SummaryTextContent>
       </SummaryCard>
 
       <SummaryCard variant="green">
@@ -25,7 +41,7 @@ export function Summary() {
           <SummarySpan>Total</SummarySpan>
           <CurrencyDollar size={32} color="#00b37e"/>
         </SummaryHeader>
-          <SummaryTextContent>R$ 10.000,00</SummaryTextContent>
+          <SummaryTextContent>{summary.total}</SummaryTextContent>
       </SummaryCard>
     </SummaryContainer>
   )
