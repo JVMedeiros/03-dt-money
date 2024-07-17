eee99a2071f330ed50330051bb135ef7d515ba59
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-07-16 22:10:27 -0300

    feat: implementing transaction context

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,12 @@
+import { useContext } from "react";
 import { Header } from "../../components/Header/index";
 import { SearchForm } from "../../components/SearchForm";
 import { Summary } from "../../components/Summary";
+import { TransactionsContext } from "../../context/TransactionContext";
 import { PriceHighlight, TransactionsContainer, TransactionsMainContainer, TransactionsTable, TransactionsTableBody, TransactionsTableData, TransactionsTableRow } from "./styles";
 
-export function Transactions() {  
+export function Transactions() {
+  const { transactions } = useContext(TransactionsContext)
   return (
     <TransactionsMainContainer>
       <Header />
@@ -12,27 +15,20 @@ export function Transactions() {
         <SearchForm />
         <TransactionsTable>
           <TransactionsTableBody>
-            <TransactionsTableRow>
-              <TransactionsTableData width="40%">Desenvolvimento de site</TransactionsTableData>
-              <TransactionsTableData>
-                <PriceHighlight variant="income">
-                  R$ 12.000,00
-                </PriceHighlight>
-              </TransactionsTableData>
-              <TransactionsTableData>Venda</TransactionsTableData>
-              <TransactionsTableData>13/04/2022</TransactionsTableData>
-            </TransactionsTableRow>
-
-            <TransactionsTableRow>
-              <TransactionsTableData width="40%">Hamburguer</TransactionsTableData>
-              <TransactionsTableData>
-                <PriceHighlight variant="outcome">
-                  - R$ 59,00
-                </PriceHighlight>
-              </TransactionsTableData>
-              <TransactionsTableData>Alimentação</TransactionsTableData>
-              <TransactionsTableData>10/04/2022</TransactionsTableData>
-            </TransactionsTableRow>
+            {transactions.map(transaction => {
+              return (
+                <TransactionsTableRow key={transaction.id}>
+                  <TransactionsTableData width="50%">{transaction.description}</TransactionsTableData>
+                  <TransactionsTableData>
+                    <PriceHighlight variant={transaction.type}>
+                      {transaction.price}
+                    </PriceHighlight>
+                  </TransactionsTableData>
+                  <TransactionsTableData>{transaction.category}</TransactionsTableData>
+                  <TransactionsTableData>{transaction.createdAt}</TransactionsTableData>
+                </TransactionsTableRow>
+              )
+            })}
           </TransactionsTableBody>
         </TransactionsTable>
       </TransactionsContainer>
