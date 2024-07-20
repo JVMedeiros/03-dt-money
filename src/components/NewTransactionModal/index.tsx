9560fc0306f2968e55d36bb7328adb20f35b5656
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-07-20 01:34:45 -0300

    feat: implementing create new transaction

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,6 +4,8 @@ import { CloseButton, Content, Overlay, TransactionModalButton, TransactionModal
 import { Controller, useForm } from 'react-hook-form';
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useContext } from 'react';
+import { TransactionsContext } from '../../context/TransactionContext';
 
 const newTransactionFormSchema = z.object({
   description: z.string(),
@@ -15,19 +17,27 @@ const newTransactionFormSchema = z.object({
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>;
 
 export function NewTransactionModal() {
+  const { createTransaction } = useContext(TransactionsContext)
   const {
     control,
     register,
     handleSubmit,
-    formState: { isSubmitting }
+    formState: { isSubmitting },
+    reset
   } = useForm<NewTransactionFormInputs>({
     resolver: zodResolver(newTransactionFormSchema),
   })
 
   async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
-    await new Promise(resolve => setTimeout(resolve, 2000));
-
-    console.log(data);
+    const { category, description, price, type } = data;
+    
+    await createTransaction({
+      category, 
+      description, 
+      price, 
+      type
+    })
+    reset();
   }
 
   return (
